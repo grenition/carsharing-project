a13e3a6d6376f0ee8dc32fae60f6b6fc73817f3c
Arseniy Mikheev <arseniy.mikhep@gmail.com>
2025-05-24 01:24:30 +0300

    feat: setup hilt

---
+++ mobile-app/app/src/main/java/com/example/easydrive/EasyDriveApp.java
@@ -0,0 +1,8 @@
+package com.example.easydrive;
+
+import android.app.Application;
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class EasyDriveApp extends Application {
+} 
---
+++ mobile-app/app/src/main/java/com/example/easydrive/di/AppModule.java
@@ -0,0 +1,11 @@
+package com.example.easydrive.di;
+
+import dagger.Module;
+import dagger.hilt.InstallIn;
+import dagger.hilt.components.SingletonComponent;
+
+@Module
+@InstallIn(SingletonComponent.class)
+public class AppModule {
+    // Provide your dependencies here
+} 
