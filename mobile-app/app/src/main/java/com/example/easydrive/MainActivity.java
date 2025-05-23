3f1bfd4478bade0388211cb71384492872833d3a
Arseniy Mikheev <arseniy.mikhep@gmail.com>
2025-05-24 01:34:43 +0300

    feat: added login fragment

--- mobile-app/app/src/main/java/com/example/easydrive/MainActivity.java
+++ mobile-app/app/src/main/java/com/example/easydrive/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.easydrive.fragments.LoginFragment;
+
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
@@ -22,5 +25,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentContainer, new LoginFragment())
+                .commit();
+        }
     }
 }
